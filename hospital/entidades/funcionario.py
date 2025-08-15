e0924b8f9df8e0dc736dde3a1f4dd05a68c04e17
VtrGbr <vgso.trab@gmail.com>
2025-08-15 14:01:27 +0000

    adicionando novos funcionarios da saude

--- hospital/entidades/funcionario.py
+++ hospital/entidades/funcionario.py
@@ -1,4 +1,6 @@
 from abc import ABC, abstractmethod
+from paciente import Paciente
+paciente = Paciente()
 
 class FuncionarioSaude(ABC):
     def __init__(self, nome, registro):
@@ -6,9 +8,17 @@ class FuncionarioSaude(ABC):
         self.registro = registro
 
     @abstractmethod
-    def requisitar_exame(self,paciente,exame):
+    def requisitarExame(self,paciente,exame):
+        pass
+    
+    @abstractmethod
+    def atenderPaciente(self,nome):
         pass
 
+   
+    def registrarProntuario(self,profissional,descricao):
+        paciente.adicionar_prontuario(profissional,descricao)
+        print("Prontuario registrado")
     def __str__(self):
         return f"{self.nome} ({self.registro})"
 
@@ -16,6 +26,9 @@ class Medico(FuncionarioSaude):
     def __init__(self, nome, registro, especialidade):
         super().__init__(nome, registro)
         self.especialidade = especialidade
+    #O nome seria o do paciente
+    def atenderPaciente(self, nome):
+        print(f"Diagnosticando o paciente: {nome}")
 
     def requisitar_exame(self, paciente, exame):
         paciente.solicitar_exame(exame)
@@ -23,14 +36,25 @@ class Medico(FuncionarioSaude):
     def receitar_medicamento(self, paciente, receita):
         paciente.adicionar_receita(receita)
 
+    def requisitarExame(self, exame):
+        paciente.solicitar_exame(exame)
+
 class Enfermeiro(FuncionarioSaude):
     def __init__(self, nome, registro):
         super().__init__(nome, registro)
 
-    def requisitar_exame(self, paciente, exame):
-        #Por enquanto nao vai ter nada
-        pass
+    def requisitar_exame(self, exame):
+        paciente.solicitar_exame(exame)
     
+    def atenderPaciente(self, nome):
+        print(f"Checando os sinais vitais do paciente {nome}")
+    def registrarProntuario(self, profissional, descricao):
+        paciente.adicionar_prontuario(profissional,descricao)
+        
+class Dentista(FuncionarioSaude):
+    pass
 
+class Psicologo(FuncionarioSaude):
+    pass
 
 
